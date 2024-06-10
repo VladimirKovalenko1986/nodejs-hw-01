04414b8522f06ebdd032eb2db08f236d6215560b
Volodimir Kovalenko <volodimirkovalenko@Volodimirs-MacBook-Pro.local>
2024-06-10 23:43:21 +0300

    add files

--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    const updetedContacts = [];
+    await fs.writeFile(PATH_DB, JSON.stringify(updetedContacts));
+    console.log('All contacts clear');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 await removeAllContacts();
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,18 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
-import { createFakeContact } from '../utils/createFakeContact.js';
 import fs from 'node:fs/promises';
 
-export const addOneContact = async () => {
+export const removeLastContact = async () => {
   try {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
-    const newContact = createFakeContact();
-    contacts.push(newContact);
+    contacts.splice(contacts.length - 1);
     await fs.writeFile(PATH_DB, JSON.stringify(contacts));
-    console.log('Contact added');
+    console.log('Last contact delete');
   } catch (error) {
     console.log(error);
   }
 };
 
-await addOneContact();
+removeLastContact();
